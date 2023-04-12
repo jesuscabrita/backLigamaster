785aee0ca1ca8eca26f3c619c37f92969f682750
Jesus Cabrita <jesusarnaldo115@gmail.com>
2023-04-12 10:50:29 -0300

    inicial

--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@ import __dirname from "./utils.js";
 import { PORT } from "./config.js";
 import { connectToDatabase } from "./database/database.js";
 import ligaRouter from "./router/equipos.router.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static(`${__dirname}/public`));
 
-app.use("/api/liga", ligaRouter);
+app.use("/api/liga", cors(), ligaRouter);
 
 app.listen(PORT, () => {
     console.log(`Servidor corre en el puerto ${PORT}`);
